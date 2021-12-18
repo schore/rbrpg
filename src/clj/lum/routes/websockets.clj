a89557d24511beaac6740046617654baa6c1692c
schore <schore.r@gmx.de>
2021-12-18 21:26:47 +0100

    Channel gets now also closed

--- src/clj/lum/routes/websockets.clj
+++ src/clj/lum/routes/websockets.clj
@@ -4,42 +4,31 @@
    [cheshire.core :as json]
    [clojure.walk]
    [lum.game.gamelogic :as gamelogic]
-   [clojure.core.async :as a :refer [chan go go-loop <! >! close!]]
+   [clojure.core.async :as a]
    [clojure.tools.logging :as log]
    [clojure.string]))
 
 
-(defn connect!
-  [in out]
-  (fn [chan]
-    (log/info "Channel open")
-    (go (>! in [:initialize]))
-    (go-loop []
-      (when-let [msg (<! out)]
-        (send! chan (json/generate-string msg))
-        (recur)))))
-
-(defn disconnect!
-  [_ status]
-  (log/info "Channel closed" status))
-
-(defn handle-receive!
-  [in]
-  (fn
-    [_ message]
-    (log/info message)
-    (let [message (json/parse-string-strict message)]
-      (go (>! in message)))))
 
 (defn ws-handler [request]
   (if-not (:websocket? request)
     {:status 200 :body "Game channel"}
-    (let [in (chan)
+    (let [in (a/chan)
           out (gamelogic/game-master in)]
       (as-channel request
-                  {:on-open (connect! in out)
-                   :on-close disconnect!
-                   :on-receive (handle-receive! in)}))))
+                  {:on-open (fn [chan]
+                              (log/info "Channel open")
+                              (a/put! in [:initialize])
+                              (a/go-loop []
+                                (when-let [msg (a/<! out)]
+                                  (send! chan (json/generate-string msg))
+                                  (recur))))
+                   :on-close (fn [_ status]
+                               (log/info status)
+                               (a/close! in))
+                   :on-receive (fn [_ message]
+                                 (log/info message)
+                                 (a/put! in (json/parse-string message)))}))))
 
 (def ws-routes
   ["/game/ws" ws-handler])

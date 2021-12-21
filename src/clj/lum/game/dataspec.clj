f3ebe22458a2e9eea5488596a60aa8fec1e74b29
schore <schore.r@gmx.de>
2021-12-21 22:05:13 +0100

    Added spec for fights

--- src/clj/lum/game/dataspec.clj
+++ src/clj/lum/game/dataspec.clj
@@ -19,31 +19,55 @@
 
 (s/def :player/xp nat-int?)
 
-(s/def :player/stat (s/and (s/cat :current nat-int? :max nat-int?)
+(s/def :game/stat (s/and (s/cat :current nat-int? :max nat-int?)
                            #(<= (:current %) (:max %))))
 
-(s/def :player/hp :player/stat)
-(s/def :player/mp :player/stat)
+(s/def :player/hp :game/stat)
+(s/def :player/mp :game/stat)
 
 (s/def :game/player (s/keys :req-un [:game/position
                                      :player/xp
                                      :player/hp
                                      :player/mp]))
 
-(s/def :npc/type #{:elf
-                   :monster})
+(s/def :enemy/hp :game/stat)
+(s/def :enemy/mp :game/stat)
 
-(s/def :game/npc (s/keys :req-un [:game/position
-                                  :npc/type]))
+(s/def :fight/enemy (s/keys :req-un [:enemy/hp
+                                     :enemy/mp]))
 
-(s/def :game/npcs (s/coll-of :game/npc))
+
+
+(s/def :effect/target #{:player :enemy})
+(s/def :effect/stat #{:hp :mp})
+(s/def :effect/n int?)
+
+(s/def :fight/action (s/cat :message string?
+                            :effects (s/coll-of (s/keys :req-un [:effect/target
+                                                                 :effect/stat
+                                                                 :effect/n]))))
+
+(s/def :fight/actions (s/coll-of :fight/action))
+
+(s/def :game/fight (s/keys :req-un [:fight/enemy
+                                    :fight/actions]))
+
 
 (s/def :game/game (s/keys :req-un [:game/player
-                                   ;;:game/npcs
-                                   :game/board]))
+                                   :game/board]
+                          :opt-un [:game/fight]))
+
+
+
+
 
 (s/explain :game/game {:board (g/get-dungeon)
                        :player {:position [1 1]
                                 :xp 0
                                 :hp [8 10]
-                                :mp [0 3]}})
+                                :mp [0 3]}
+                       :fight {:enemy {:hp [20 20]
+                                       :mp [0 0]}
+                               :actions [["Heal" [{:target :player
+                                                   :stat :hp
+                                                   :n 5}]]]}})

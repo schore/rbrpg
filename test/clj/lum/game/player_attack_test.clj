6d03afcec771e4b2fbde7b4b0fec187b341ec390
schore <schore.r@gmx.de>
2022-01-11 22:07:44 +0100

    Enhanced test functions

--- test/clj/lum/game/player_attack_test.clj
+++ test/clj/lum/game/player_attack_test.clj
@@ -15,8 +15,16 @@
 (def gamestate
   {})
 
+
+
 (deftest attack-role
+  (defn get-damage [& rolls]
+    (get-in (apply attack gamestate rolls) [1 0 :n]))
   (testing "A 20 always hits and gives double roles"
-    (is (> -1 (get-in (attack gamestate 20 1 2) [1 0 :n]))))
+    (is (> -1 (get-damage 20 1 2))))
   (testing "A 1 never hits"
-    (is (= 0 (get-in (attack gamestate 1) [1 0 :n])))))
+    (is (= 0 (get-damage 1))))
+  (testing "Miss because of AC"
+    (is (= 0 (get-damage 10))))
+  (testing "Regular hit"
+    (is (= -1 (get-damage 11 1 2)))))

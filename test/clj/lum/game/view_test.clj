2e142b9266f8096629b067fb67cbfe0b40e270a6
schore <schore.r@gmx.de>
2022-09-12 22:32:52 +0200

    More tests for undiscovered fields

--- test/clj/lum/game/view_test.clj
+++ test/clj/lum/game/view_test.clj
@@ -9,8 +9,21 @@
 (deftest can-view-field
   (doseq [[x y] [[10 10]
                  [10 15]
-                 [12 12]
+                 [13 13]
+                 [13 14]
                  [15 10]]]
     (testing (str [x y])
       (dsl/test-map-loaded 10 10)
       (is (dsl/field-visible? x y)))))
+
+(deftest can-not-view-field
+  (doseq [[x y] [[1 1]
+                 [5 1]
+                 [10 1]
+                 [14 14]
+                 [16 10]
+                 [10 16]
+                 [10 4]]]
+    (testing (str [x y])
+      (dsl/test-map-loaded 10 10)
+      (is (not (dsl/field-visible? x y))))))

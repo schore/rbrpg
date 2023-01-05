85f3090d76523e222c5bef5717e168eef5ff2c6d
schore <schore.r@gmx.de>
2023-01-05 22:31:09 +0100

    Added test report generation based on allure

--- .jenkins/test.sh
+++ .jenkins/test.sh
@@ -11,8 +11,24 @@ run_tests () {
     return $result
 }
 
+generate_allure_report () {
+    local execution_dir=$1
+    local test_xunit_dir=$2
+    local allure=../allure/allure-2.20.1/bin/allure
+
+    pushd $1
+    if [ -d $test_xunit_dir/allure-report/history ]
+    then
+        cp -r $test_xunit_dir/allure-report/history $test_xunit_dir
+    fi
+
+    $allure generate -c $test_xunit_dir -o $test_xunit_dir/allure-report
+    popd
+}
+
 if [ $0 == ${BASH_SOURCE} ]
 then
     run_tests "$(dirname "$0")/.."
     exit $?
 fi
+
--- test-and-publish.sh
+++ test-and-publish.sh
@@ -1,6 +1,19 @@
-#!/bin/bash -e
+#!/bin/bash
 #
-cd "$(dirname "$0")/.jenkins"
+pushd "$(dirname "$0")"
 
-./test.sh
-./publish.sh
+source .jenkins/test.sh
+
+
+
+run_tests .
+testresult=$?
+generate_allure_report . ./target/test-reports
+
+if  [[ $testresult == 0 ]]
+then
+    echo "Publish to github pages"
+    .jenkins/publish.sh
+fi
+
+popd

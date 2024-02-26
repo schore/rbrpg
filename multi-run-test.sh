6fcc7bbd859f0e8c31bf960173939ba5d88c9b03
schore <schore.r@gmx.de>
2024-02-26 20:14:54 +0100

    Fixed test loop run

--- multi-run-test.sh
+++ multi-run-test.sh
@@ -4,8 +4,10 @@ pushd "$(dirname "$0")"
 
 source .jenkins/test.sh
 
-for i in {0..$1}
+for i in $(seq 1 $1);
 do
+    echo "Run: $i"
+    echo "########"
     rm ./target/test-reports/*.xml
     run_tests .
     generate_allure_report . ./target/test-reports
